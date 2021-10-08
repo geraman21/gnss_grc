e4ee46ba6deb6df1f364fbb204b411419d089445
German Abdurahmanov <german.abdurahmanov@aldi-sued.com>
2021-10-08 14:11:02 +0200

    Change helper function ace namings

--- lib/helper-functions.h
+++ lib/helper-functions.h
@@ -34,8 +34,9 @@ std::vector<double> getPseudoRanges(std::vector<double> &travelTime, double star
 void custom_fft(std::valarray<std::complex<double>> &x);
 void custom_ifft(std::valarray<std::complex<double>> &x);
 // Returns codePhase and strength of a Channel provided its caCodeVector
-std::tuple<int, float> getChannelStrength(float ts, std::vector<std::complex<float>> &caCodeVector,
-                                          std::vector<float> &longSignal);
+std::tuple<int, float> doParallelCodePhaseSearch(float ts,
+                                                 std::vector<std::complex<float>> &caCodeVector,
+                                                 std::vector<float> &longSignal);
 AcqResults performAcquisition(int PRN, float ts, std::vector<std::complex<float>> &caCodeVector,
                               std::vector<float> &longSignal);
 AcqResults checkIfChannelPresent(int PRN, float ts, std::vector<std::complex<float>> &caCodeVector,
